6233167e8ea259ae26e4cf28eb86b15b4f5b5e2d
Pawel Szulc <paul.szulc@gmail.com>
2019-08-16 13:05:40 +0200

    Add ability to list all Todos (with their keys)

--- src/Todo.hs
+++ src/Todo.hs
@@ -3,6 +3,7 @@ module Todo where
 import Polysemy
 import KVS
 import MonotonicSequence
+import qualified Data.Map.Strict as M
 
 type Key = Int
 
@@ -19,3 +20,6 @@ add todo = do
   key <- next
   insertKvs key todo
   return key
+
+list :: Member (KVS Key Todo) r => Sem r (M.Map Key Todo)
+list = fmap M.fromList listAllKvs
