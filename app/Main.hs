b5ce6ca1d39f47e2cd9f88895b87a5f94953575b
Pawel Szulc <paul.szulc@gmail.com>
2019-08-21 01:03:58 +0200

    Create main app that serves the server for the TODO api

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.IORef
+import Data.Function ((&))
+import           KVS
+import MonotonicSequence
+import qualified           Network.Wai.Handler.Warp as W
+import           Polysemy
+import           Polysemy.Error
+import           Polysemy.State
+import           Servant.Server
+import           Todo
+import           TodoRest
+import qualified Data.Map.Strict as M
+import Control.Monad.Except
+
+initTodos :: M.Map Key Todo
+initTodos = (M.singleton 1 (newTodo "push to repo"))
+
+createApp :: IO Application
+createApp = do
+  idIORef <- newIORef 0
+  kvsIORef <- newIORef initTodos
+  return (serve api $ hoistServer api (\sem -> interpretServer sem idIORef kvsIORef) server)
+  where
+    interpretServer sem idIORef kvsIORef =  sem
+                & runKvsOnMapState
+                & runMonotonicSequenceOnState
+                & runStateIORef @Int idIORef 
+                & runStateIORef @(M.Map Key Todo) kvsIORef
+                & runError @TodoError
+                & runM
+                & liftToHandler
+    liftToHandler = Handler . ExceptT . (fmap handleErrors)
+    handleErrors (Left (TodoNotAvailable id)) = Left err404 { errBody = "Todo does not exist" }
+    handleErrors (Right value) = Right value
+
 main :: IO ()
-main = undefined
+main = do
+  app <- createApp
+  W.run 9090 app
